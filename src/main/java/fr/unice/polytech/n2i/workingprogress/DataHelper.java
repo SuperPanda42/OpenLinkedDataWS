f5326e493ea9a22d0b41c5e66e150c5d0c6444d7
clement0210 <duffau@polytech.unice.fr>
2014-12-05 05:16:21 +0100

    Second version

--- src/main/java/fr/unice/polytech/n2i/workingprogress/DataHelper.java
+++ src/main/java/fr/unice/polytech/n2i/workingprogress/DataHelper.java
@@ -14,16 +14,26 @@ import java.util.Random;
 public class DataHelper {
 
 
-    public static Question getQuestion(){
-        int rand=new Random().nextInt(2);
-        switch (rand){
-            case 0 : return getQuestionGDP();
-            case 1 : return getQuestionNumberOfPeople();
-            default : return getQuestionGDP();
+    private static Question questionCasualties;
+
+    public static Question getQuestion() {
+        int rand = new Random().nextInt(4);
+        switch (rand) {
+            case 0:
+                return getQuestionGDP();
+            case 1:
+                return getQuestionNumberOfPeople();
+            case 2:
+                return getQuestionHDI();
+            case 3:
+                return getQuestionCasualties();
+            default:
+                return getQuestionGDP();
         }
     }
-    private static Question getQuestionNumberOfPeople(){
-        String query2="PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+
+    private static Question getQuestionNumberOfPeople() {
+        String query2 = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
                 "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
                 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
                 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -59,38 +69,35 @@ public class DataHelper {
         //after it goes standard query execution and result processing which can
         // be found in almost any Jena/SPARQL tutorial.
         try {
-            List<Question> questions=new ArrayList<>();
+            List<Question> questions = new ArrayList<Question>();
             ResultSet results = qexec.execSelect();
-            for (; results.hasNext();) {
-                QuerySolution solution=results.next();
-                String str2= solution.get("?nb").asLiteral().getString();
-                String str=solution.get("?country").asLiteral().getString();
+            for (; results.hasNext(); ) {
+                QuerySolution solution = results.next();
+                String str2 = solution.get("?nb").asLiteral().getString();
+                String str = solution.get("?country").asLiteral().getString();
                 // Result processing is done here.
-                Question question=new Question();
-                question.setQuestion("What's the population census on "+str+" ?");
+                Question question = new Question();
+                question.setQuestion("What's the population census on " + str + " ?");
                 question.setRightAnswer(str2);
                 questions.add(question);
             }
-            int rand=new Random().nextInt(questions.size());
+            int rand = new Random().nextInt(questions.size());
 
-            Question question=questions.get(rand);
+            Question question = questions.get(rand);
 
-            List<String> answers=new ArrayList<String>();
+            List<String> answers = new ArrayList<String>();
             answers.add(question.getRightAnswer());
-            for(int i=0;i<4;i++){
-                rand=new Random().nextInt(questions.size());
+            for (int i = 0; i < 4; i++) {
+                rand = new Random().nextInt(questions.size());
                 answers.add(questions.get(rand).getRightAnswer());
             }
             Collections.shuffle(answers);
             question.setAnswers(answers);
             return question;
 
-        }
-
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             qexec.close();
         }
         return new Question();
@@ -99,7 +106,7 @@ public class DataHelper {
 
 
     private static Question getQuestionGDP() {
-        String query2="PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+        String query2 = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
                 "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
                 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
                 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -125,7 +132,8 @@ public class DataHelper {
 
 
     }
-    private static Question queryAQuestionGDP(String queryString){
+
+    private static Question queryAQuestionGDP(String queryString) {
         // now creating query object
         Query query = QueryFactory.create(queryString);
         // initializing queryExecution factory with remote service.
@@ -134,30 +142,30 @@ public class DataHelper {
         //after it goes standard query execution and result processing which can
         // be found in almost any Jena/SPARQL tutorial.
         try {
-            List<Question> questions=new ArrayList<>();
+            List<Question> questions = new ArrayList<Question>();
             ResultSet results = qexec.execSelect();
-            for (; results.hasNext();) {
-                QuerySolution solution=results.next();
-                String str2= solution.get("?pib").asLiteral().getString();
-                String str=solution.get("?country").asLiteral().getString();
+            for (; results.hasNext(); ) {
+                QuerySolution solution = results.next();
+                String str2 = solution.get("?pib").asLiteral().getString();
+                String str = solution.get("?country").asLiteral().getString();
                 // Result processing is done here.
-                Question question=new Question();
-                question.setQuestion("What's the Gross Domestic Product for "+str+" ?");
+                Question question = new Question();
+                question.setQuestion("What's the Gross Domestic Product for " + str + " ?");
                 question.setRightAnswer(str2);
                 questions.add(question);
             }
-            int rand=new Random().nextInt(questions.size());
+            int rand = new Random().nextInt(questions.size());
 
-            Question question=questions.get(rand);
-            while(!question.getRightAnswer().contains("E")){
-                rand=new Random().nextInt()*questions.size();
-                question=questions.get(rand);
+            Question question = questions.get(rand);
+            while (!question.getRightAnswer().contains("E")) {
+                rand = new Random().nextInt() * questions.size();
+                question = questions.get(rand);
             }
-            List<String> answers=new ArrayList<String>();
-            for(int i=-2;i<2;i++){
-                if(question.getRightAnswer().contains("E")){
-                    String[] spliter=question.getRightAnswer().split("E");
-                    answers.add(spliter[0]+"E"+(Integer.parseInt(spliter[1])+i));
+            List<String> answers = new ArrayList<String>();
+            for (int i = -2; i < 2; i++) {
+                if (question.getRightAnswer().contains("E")) {
+                    String[] spliter = question.getRightAnswer().split("E");
+                    answers.add(spliter[0] + "E" + (Integer.parseInt(spliter[1]) + i));
                 }
 
             }
@@ -165,12 +173,148 @@ public class DataHelper {
             question.setAnswers(answers);
             return question;
 
-        }
-
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            qexec.close();
         }
-        finally {
+        return new Question();
+    }
+
+    public static Question getQuestionHDI() {
+        String query2 = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n" +
+                "PREFIX : <http://dbpedia.org/resource/>\n" +
+                "PREFIX prop: <http://dbpedia.org/property/>\n" +
+                "PREFIX dbpedia: <http://dbpedia.org/>\n" +
+                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+                "\n" +
+                "select * \n" +
+                "where {\n" +
+                "  service <http://dbpedia.org/sparql/> {\n" +
+                "     select ?country ?hdi where {\n" +
+                "\n" +
+                "       ?c prop:hdi ?hdi;\n" +
+                "          prop:commonName ?country.\n" +
+                "     } \n" +
+                "     limit 100\n" +
+                "  }\n" +
+                "}\n";
+        return queryAQuestionHDI(query2);
+    }
+
+    private static Question queryAQuestionHDI(String queryString) {
+        // now creating query object
+        Query query = QueryFactory.create(queryString);
+        // initializing queryExecution factory with remote service.
+        // **this actually was the main problem I couldn't figure out.**
+        QueryExecution qexec = QueryExecutionFactory.create(query, DatasetFactory.createMem());
+        //after it goes standard query execution and result processing which can
+        // be found in almost any Jena/SPARQL tutorial.
+        try {
+            List<Question> questions = new ArrayList<Question>();
+            ResultSet results = qexec.execSelect();
+            for (; results.hasNext(); ) {
+                QuerySolution solution = results.next();
+                String str2 = solution.get("?hdi").asLiteral().getString();
+                String str = solution.get("?country").asLiteral().getString();
+                // Result processing is done here.
+                Question question = new Question();
+                question.setQuestion("What's the human development index for " + str + " ?");
+                question.setRightAnswer(str2);
+                questions.add(question);
+            }
+            int rand = new Random().nextInt(questions.size());
+
+            Question question = questions.get(rand);
+
+            List<String> answers = new ArrayList<String>();
+            answers.add(question.getRightAnswer());
+            for (int i = 0; i < 4; i++) {
+                rand = new Random().nextInt(questions.size());
+                answers.add(questions.get(rand).getRightAnswer());
+            }
+            Collections.shuffle(answers);
+            question.setAnswers(answers);
+            return question;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            qexec.close();
+        }
+        return new Question();
+    }
+
+    public static Question getQuestionCasualties() {
+        String query2 = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n" +
+                "PREFIX : <http://dbpedia.org/resource/>\n" +
+                "PREFIX prop: <http://dbpedia.org/property/>\n" +
+                "PREFIX ont: <http://dbpedia.org/ontology/>\n" +
+                "PREFIX dbpedia: <http://dbpedia.org/>\n" +
+                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+                "\n" +
+                "select * \n" +
+                "where {\n" +
+                "  service <http://dbpedia.org/sparql/> {\n" +
+                "     select (max(?nb) as ?number)  ?name where {\n" +
+                "       ?c rdf:type ont:MilitaryConflict;\n" +
+                "           prop:casualties ?nb.\n" +
+                "      ?c foaf:name ?name.\n" +
+                "       Filter ( datatype(?nb) = xsd:integer && ?nb >= 10000)\n" +
+                "     } \n" +
+                "group by ?name\n" +
+                "     limit 100\n" +
+                "  }\n" +
+                "}\n";
+        return queryAQuestionCasualties(query2);
+    }
+
+    private static Question queryAQuestionCasualties(String queryString) {
+        // now creating query object
+        Query query = QueryFactory.create(queryString);
+        // initializing queryExecution factory with remote service.
+        // **this actually was the main problem I couldn't figure out.**
+        QueryExecution qexec = QueryExecutionFactory.create(query, DatasetFactory.createMem());
+        //after it goes standard query execution and result processing which can
+        // be found in almost any Jena/SPARQL tutorial.
+        try {
+            List<Question> questions = new ArrayList<Question>();
+            ResultSet results = qexec.execSelect();
+            for (; results.hasNext(); ) {
+                QuerySolution solution = results.next();
+                String str2 = solution.get("?number").asLiteral().getString();
+                String str = solution.get("?name").asLiteral().getString();
+                // Result processing is done here.
+                Question question = new Question();
+                question.setQuestion("What's ast least the casualties fo the war " + str + " ?");
+                question.setRightAnswer(str2);
+                questions.add(question);
+            }
+            int rand = new Random().nextInt(questions.size());
+
+            Question question = questions.get(rand);
+
+            List<String> answers = new ArrayList<String>();
+            answers.add(question.getRightAnswer());
+            for (int i = 0; i < 4; i++) {
+                rand = new Random().nextInt(questions.size());
+                answers.add(questions.get(rand).getRightAnswer());
+            }
+            Collections.shuffle(answers);
+            question.setAnswers(answers);
+            return question;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             qexec.close();
         }
         return new Question();
